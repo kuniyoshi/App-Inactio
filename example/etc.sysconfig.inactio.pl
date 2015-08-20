f75b9f28051082c8f19a89091f9b0205a7a64f29
Kuniyoshi Kouji <kuniyoshi@seesaa.co.jp>
2015-08-20 21:04:59 +0900

    add example config

---
+++ example/etc.sysconfig.inactio.pl
@@ -0,0 +1,9 @@
+use utf8;
+{
+    organization => "your organization here",
+    topic_name   => "このトピックを閉じて一斉架電を止めてください",
+
+    "example.com" => {
+        api_key => "your api key of the example.com",
+    },
+};
